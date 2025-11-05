ec8d782b29b5d0d53fc40fb4fb86fb0769786164
hemingus <heming.hanevik@gmail.com>
2025-11-05 16:48:37 +0100

    many filter by category

--- src/components/Meny/Meny.tsx
+++ src/components/Meny/Meny.tsx
@@ -13,11 +13,33 @@ interface MenyProps {
     data: Array<MenuItem>
 }
 
+type Kategori = "Hovedrett" | "Forrett" | "Dessert";
+
 export default function Meny({ data }: MenyProps) {
+    const hovedretter = data.filter(item => item.kategori === "Hovedrett")
+    console.log(hovedretter)
+    const forretter = data.filter(item => item.kategori === "Forrett")
+    console.log(forretter)
+    const desserter = data.filter(item => item.kategori === "Forrett")
+    console.log(desserter)
+
     return (
-        <div>
+        <div className="meny-container">
+            <h2>Forretter</h2>
             <ul className="meny-list">
-                {data.map(item => 
+                {forretter.map(item => 
+                    <MenyCard key={item.id} {...item}/>
+                )}
+            </ul>
+            <h2>Hovedretter</h2>
+            <ul className="meny-list">
+                {hovedretter.map(item => 
+                    <MenyCard key={item.id} {...item}/>
+                )}
+            </ul>
+            <h2>Desserter</h2>
+            <ul className="meny-list">
+                {desserter.map(item => 
                     <MenyCard key={item.id} {...item}/>
                 )}
             </ul>
