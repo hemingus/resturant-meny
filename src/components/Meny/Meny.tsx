4683c9c027714ec2a9481174d73d4147d4c62ca0
hemingus <heming.hanevik@gmail.com>
2025-11-06 10:13:29 +0100

    fix structure and add classes to Meny

--- src/components/Meny/Meny.tsx
+++ src/components/Meny/Meny.tsx
@@ -13,36 +13,32 @@ interface MenyProps {
     data: Array<MenuItem>
 }
 
-type Kategori = "Hovedrett" | "Forrett" | "Dessert";
-
 export default function Meny({ data }: MenyProps) {
-    const hovedretter = data.filter(item => item.kategori === "Hovedrett")
-    console.log(hovedretter)
-    const forretter = data.filter(item => item.kategori === "Forrett")
-    console.log(forretter)
-    const desserter = data.filter(item => item.kategori === "Forrett")
-    console.log(desserter)
-
+ 
     return (
-        <div className="meny-container">
-            <h2>Forretter</h2>
-            <ul className="meny-list">
-                {forretter.map(item => 
-                    <MenyCard key={item.id} {...item}/>
-                )}
-            </ul>
-            <h2>Hovedretter</h2>
-            <ul className="meny-list">
-                {hovedretter.map(item => 
-                    <MenyCard key={item.id} {...item}/>
-                )}
-            </ul>
-            <h2>Desserter</h2>
-            <ul className="meny-list">
-                {desserter.map(item => 
-                    <MenyCard key={item.id} {...item}/>
-                )}
-            </ul>
-        </div>
+        <section className="meny-container">
+            <div className="meny-list">
+                <h2>Hovedretter</h2>
+                <ul className="meny-list">
+                    {data.filter(item => item.kategori === "Hovedrett").map(item =>
+                        <MenyCard key={item.id} {...item}/>
+                    )}
+                </ul>
+            </div>
+            <div className="meny-list">
+                <h2>Forretter</h2>
+                <ul className="meny-list">
+                    {data.filter(item => item.kategori === "Forrett").map(item =>
+                        <MenyCard key={item.id} {...item}/>
+                    )}
+                </ul>
+                <h2>Desserter</h2>
+                <ul className="meny-list">
+                    {data.filter(item => item.kategori === "Dessert").map(item =>
+                        <MenyCard key={item.id} {...item}/>
+                    )}
+                </ul>
+            </div>
+        </section>
     )
 }
